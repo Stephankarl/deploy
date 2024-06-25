ce4d290106c6da86682b12368b6e05b5b80c066e
Stephan Karl <steph@Stephans-MBP.localdomain>
2024-06-25 11:23:18 -0700

    deploy production test 1 backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+const people = [
+    { name: 'John', age: 30, id: 1 },
+    { name: 'Jane', age: 25, id: 2 },
+    { name: 'Jim', age: 40, id: 3 },
+];
+
 app.get('/', (req, res) => {
-    res.send('Hello World!!');
+    res.send('API is running');
+});
+
+app.get('/people', (req, res) => {
+    console.log('hit')
+    res.json(people);
 });
 
 app.listen(8000, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,31 @@
+import { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 function App() {
+
+  const [people, setPeople] = useState([])
+  
+  useEffect(() => {
+    getPeople();
+  }, [])
+  
+  const getPeople = async () => {
+    const response = await axios.get('/people');
+    setPeople(response.data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Deploy!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {people.map(person => (
+            <li key={person.id}>{person.name} is {person.age} years old</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
